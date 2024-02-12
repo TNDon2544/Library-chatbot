80242cbad8961f3b74d3ffe4908d9a6de6fb8d0a
Thanadon-Pongjessada <thanadon.pjsd@gmail.com>
2024-02-12 18:06:24 +0700

    add:loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import icitLogo from "../assets/icit-logo.png";
 import { logout, isUserLoggedIn } from "../service/auth";
 import { useEffect, useState } from "react";
+import "../css/loading.css";
 
 function Navbar() {
   const name = localStorage.getItem("name");
@@ -20,32 +21,38 @@ function Navbar() {
   };
 
   return (
-    <div className="h-full w-full py-1 shadow-[0px_5px_10px_0px_#62618E40]">
-      <div className="container mx-auto px-2 md:px-6">
-        <div className="flex justify-between items-center">
-          <img
-            src={icitLogo}
-            width={80}
-            height={80}
-            alt="icit-logo"
-            className="select-none"
-          />
-          <div className="flex items-center gap-2 md:gap-5">
-            <p className="text-[16px] md:text-[18px] font-[500] text-[#004370]">
-              {name ? name : ""}
-            </p>
-            <button
-              onClick={handleLogout}
-              className={`${
-                isLogin ? "" : "hidden"
-              } text-[14px] md:text-[16px] h-full px-3 py-1  bg-[#1f5e95] text-white rounded-[18px]`}
-            >
-              ออกจากระบบ
-            </button>
+    <>
+      <div className={`${isLogin ? "hidden" : ""} loader-wrapper`}>
+        <span className="loader" />
+      </div>
+
+      <div className="h-full w-full py-1 shadow-[0px_5px_10px_0px_#62618E40]">
+        <div className="container mx-auto px-2 md:px-6">
+          <div className="flex justify-between items-center">
+            <img
+              src={icitLogo}
+              width={80}
+              height={80}
+              alt="icit-logo"
+              className="select-none"
+            />
+            <div className="flex items-center gap-2 md:gap-5">
+              <p className="text-[16px] md:text-[18px] font-[500] text-[#004370]">
+                {name ? name : ""}
+              </p>
+              <button
+                onClick={handleLogout}
+                className={`${
+                  isLogin ? "" : "hidden"
+                } text-[14px] md:text-[16px] h-full px-3 py-1  bg-[#1f5e95] text-white rounded-[18px]`}
+              >
+                ออกจากระบบ
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
