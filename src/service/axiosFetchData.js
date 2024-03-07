ffd7219f036f7bf5578121412f67a71338da28ad
Thanadon-Pongjessada <thanadon.pjsd@gmail.com>
2024-03-07 16:21:29 +0700

    fix fetch data service

--- src/service/axiosFetchData.js
+++ src/service/axiosFetchData.js
@@ -1,33 +1,31 @@
 import axios from "axios";
 
-export const getMethod = (endpoint) => {
-  return axios
-    .get(`${import.meta.env.VITE_API_URL}${endpoint}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    })
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      console.log(endpoint, error);
-      throw error;
-    });
+export const getMethod = async (endpoint) => {
+  try {
+    const response = await axios
+      .get(`${import.meta.env.VITE_API_URL}${endpoint}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+    return response;
+  } catch (error) {
+    console.log(endpoint, error);
+    throw error;
+  }
 };
 
-export const postMethod = (endpoint, body, bearer) => {
-  return axios
-    .post(`${import.meta.env.VITE_API_URL}${endpoint}`, body, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem(bearer)}`,
-      },
-    })
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      console.log(endpoint, error);
-      throw error;
-    });
+export const postMethod = async (endpoint, body, bearer) => {
+  try {
+    const response = await axios
+      .post(`${import.meta.env.VITE_API_URL}${endpoint}`, body, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(bearer)}`,
+        },
+      });
+    return response;
+  } catch (error) {
+    console.log(endpoint, error);
+    throw error;
+  }
 };
